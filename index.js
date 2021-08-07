b8b819731d3a52b2d36d8e34b12f92f221b2a7d4
Pablo Arak <pablo.arak.vm@gmail.com>
2021-08-07 16:56:37 -0400

    Added midlewares for errors handler

--- index.js
+++ index.js
@@ -2,10 +2,24 @@ const express = require('express');
 const app = express();
 
 const { config } = require('./config/index');
-
 const moviesApi = require('./routes/movies.js');
 
+const { logErrors, errorHandler, wrapErrors } = require('./utils/middleware/errorHandlers.js')
+const { notFoundHandler } = require('./utils/middleware/notFoundHandler.js')
+
+// Body parser
+app.use(express.json());
+
+// Routes
 moviesApi(app);
+
+// Catch 404
+app.use(notFoundHandler);
+
+// Errors middleware
+app.use(logErrors);
+app.use(wrapErrors);
+app.use(errorHandler);
 
 app.listen(config.port, () => {
   console.log(`Listening http://localhost:${config.port}`);
