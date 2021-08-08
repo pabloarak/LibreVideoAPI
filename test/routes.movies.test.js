b51cdd6ef9fc793181d7c67dccff37babf5a1c69
Pablo Arak <pablo.arak.vm@gmail.com>
2021-08-08 13:13:16 -0400

    Added test for routes

---
+++ test/routes.movies.test.js
@@ -0,0 +1,32 @@
+const assert = require('assert');
+const proxyquire = require('proxyquire');
+
+const { moviesMock, MoviesServiceMock } = require('../utils/mocks/movies.js');
+const testServer = require('../utils/testServer');
+
+// Objetive: test routes
+
+describe('routes - movies', () => {
+    const route = proxyquire('../routes/movies', {
+        '../services/movies': MoviesServiceMock // Replace de original call to services in ../routes/movies
+    });
+
+    const request = testServer(route);
+
+    describe('GET /movies', () => {
+        it('Should respond with status 200', (done) => {
+            request.get('/api/movies').expect(200, done);
+        });
+
+        it('Should respond with the list of movies', (done) => {
+            request.get('/api/movies').end((err,res) => {
+                assert.deepEqual(res.body, {
+                    data: moviesMock,
+                    message: 'Movies listed'
+                });
+
+                done();
+            });
+        });
+    });
+});
