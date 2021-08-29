402f63782798b8aed3aa0e298fb99ae9a8526ca8
Pablo Arak <pablo.arak.vm@gmail.com>
2021-08-29 17:31:50 -0400

    Added CRUD

---
+++ routes/userMovies.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const UserMoviesService = require('../services/userMovies');
+const { userIdSchema } = require('../utils/schemas/users');
+const { userMovieIdSchema, createUserMovieSchema } = require('../utils/schemas/userMovies');
+
+const validationHandler = require('../utils/middleware/validationHandler');
+
+const userMoviesApi = (app) => {
+    const router = express.Router();
+    app.use('/api/user-movies', router);
+
+    const userMoviesService = new UserMoviesService();
+
+    router.get('/', validationHandler({ userId: userIdSchema }, 'query'), async (req, res, next) => {
+
+        const { userId } = req.query;
+
+        try {
+            const userMovies = await userMoviesService.getUserMovies({ userId });
+
+            res.status(200).json({
+                data: userMovies,
+                message: 'user movies listed'
+            });
+        } catch (error) {
+            next(error);
+        }
+    });
+
+    router.post('/', validationHandler(createUserMovieSchema), async (req, res, next) => {
+        const { body: userMovie } = req;
+
+        try {
+            const createdUserMovieId = await userMoviesService.createUserMovie({
+                userMovie
+            });
+
+            res.status(201).json({
+                data: createdUserMovieId,
+                message: 'user movie created'
+            });
+        } catch (error) {
+            next(error);
+        }
+    });
+
+    router.delete('/:userMovieId', validationHandler({ userMovieId: userMovieIdSchema}, 'params'), async (req, res, next) => {
+        const { userMovieId } = req.params;
+
+        try {
+            const deletedUserMovieId = await userMoviesService.deleteUserMovie({
+                userMovieId
+            });
+
+            res.status(201).json({
+                data: deletedUserMovieId,
+                message: 'user movie deleted'
+            });
+        } catch (error) {
+            next(error);
+        }
+    });
+};
+
+module.exports = userMoviesApi;
